fdefa2b1c21f036035277808ade705a1f1d8b67a
syedsaifshah <64294500+syedsaifshah@users.noreply.github.com>
2022-12-14 23:52:54 +0500

    merge

---
+++ app/src/main/java/com/thetrusttech/getacarparts/ui/home/ui/auth/signup/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.thetrusttech.getacarparts.ui.home.ui.auth.signup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.thetrusttech.getacarparts.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
